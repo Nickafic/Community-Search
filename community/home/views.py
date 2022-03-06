acf28a67acda34cd0074c302d5e30bf67193d1f7
Nickafic <85850258+Nickafic@users.noreply.github.com>
2022-03-06 03:45:50 -0800

    staging

--- community/home/views.py
+++ community/home/views.py
@@ -30,11 +30,12 @@ def add(request):
         return redirect(request.META['HTTP_REFERER'])
 
 def results(request):
-    hivelink = ''
+    redlink = ''
     digglink = ''
+    hivelink = ''
+    linklist = [redlink,digglink,hivelink]
     digginfo = ''
     redinfo = ''
-    redlink = False
     form = CommForm()
     form2 = AddForm()
     if request.method == 'POST':
@@ -49,15 +50,13 @@ def results(request):
 
             #reddit scrape
             redinfo = redd(name)
-            form2.initial['add'] = redlink
+            linklist[0] = f'https://reddit.com/r/{name}'
+            form2.initial['add'] = linklist[0]
 
-            hivelink = f'https://hive.blog/trending/{name}'
-            redlink = f'https://reddit.com/r/{name}'
-            digglink = f'https://digg.com/{name}'
+            linklist[2] = f'https://hive.blog/trending/{name}'
+            linklist[1] = f'https://digg.com/{name}'
     return render(request, 'results.html', {
-        'hivelink': hivelink,
-        'digglink': digglink,
-        'redlink': redlink,
+        'linklist':linklist,
         'form2':form2,
         'form': form,
         'redinfo': redinfo,
